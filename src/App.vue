67898a80fd4519f65d1f696b9d133f5c0922bb01
zhumingjie1996 <1005614322@qq.com>
2021-03-25 23:22:52 +0800

    关于 computed 不能双向绑定渲染页面的解决方法

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <div></div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <test></test>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/test.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Test
   }
 }
 </script>
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,3 +1,63 @@
+/**
+computed 没有双向绑定
+computed 里面的 return 相当于 get()，是只读的
+如果要改变 computed 里面的值要用 set()
+通过 set() 改变值之后，页面上并不会渲染
+如果一定要渲染到页面上，可以使用 Vue.$forceUpdata() 进行强制渲染
+*/
+
 <template>
-    
+  <div class="test">
+    <div class="lis" v-for="(item, index) in list" :key="item.id">
+      <span>{{ item.name }}</span>
+      <input type="button" @click="btnClick(index)" />
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {};
+  },
+  computed: {
+    list: {
+      get() {
+        return [
+          {
+            name: "zhangsan",
+            id: 1,
+          },
+          {
+            name: "lisi",
+            id: 2,
+          },
+          {
+            name: "wangwu",
+            id: 3,
+          },
+        ];
+      },
+      set(i){
+          this.list[i].name += "1";
+      }
+    },
+  },
+  methods: {
+    btnClick(i) {
+    //   this.$set(this.list[i], "name", this.list[i].name + "1");
+    this.list = i;
+    this.$forceUpdate();
+    },
+  },
+  watch: {
+    list: {
+      handler(val) {
+        console.log(val);
+      },
+      deep: true,
+    },
+  },
+};
+</script>
